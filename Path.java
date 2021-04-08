85ce87dc8ae8f5c6b9d0dbf5e61f95ce6345c5dd
wHuangCA <82129384+wHuangCA@users.noreply.github.com>
2021-04-08 10:34:49 -0700

    Update Path.java

--- Path.java
+++ Path.java
@@ -18,8 +18,7 @@ public class Path {
         for (int i = 1; i < path.size(); i++) {
             Node node1 = path.get(i - 1);
             Node node2 = path.get(i);
-            int index = node1.getNeighbors().indexOf(node2);
-            distance += node1.getDistances().get(index);
+            distance += node1.getDistanceOf(node2);
         }
         return distance;
     }
@@ -48,13 +47,14 @@ public class Path {
 
     // Find the Path from start Node to destination Node.
     public static Path findPath(String fileName, String startNodeName, String destinationNodeName) {
-        LinkedList<Node> path = new LinkedList<>();
-        // Set up.
         Map map = new Map(fileName);
         if (map.getNodeByName(startNodeName) == null)
             throw new NoSuchElementException("Start Node not found; Start Node: " + startNodeName);
         if (map.getNodeByName(destinationNodeName) == null)
             throw new NoSuchElementException("Destination Node not found; Destination Node: " + destinationNodeName);
+
+        // Set up.
+        LinkedList<Node> path = new LinkedList<>();
         Node start = map.getNodeByName(startNodeName);
         Node destination = map.getNodeByName(destinationNodeName);
         path.add(start);
@@ -62,23 +62,26 @@ public class Path {
         Node nextNode;
         current.updateVisited(true);
         boolean found = false;
+
         // Traverse the Map.
         while (!found) {
             nextNode = null;
             double minDistance = Double.MAX_VALUE;
+
             // Find the unvisited neighboring Node that is closest to start Node and destination Node.
             for (int i = 0; i < current.getNeighbors().size(); i++) {
                 Node node = current.getNeighbors().get(i);
-                //int distance = start.findManhattanDistance(node) + node.findManhattanDistance(destination);
-                double distance = start.findEuclideanDistance(node) + node.findEuclideanDistance(destination);
-                if (distance < minDistance && !node.isVisited()) {
+                //int weightedDistance = start.findManhattanDistance(node) / 2 + node.findManhattanDistance(destination);
+                double weightedDistance = start.findEuclideanDistance(node) / 2 + node.findEuclideanDistance(destination);
+                if (weightedDistance < minDistance && !node.isVisited()) {
                     nextNode = node;
-                    minDistance = distance;
+                    minDistance = weightedDistance;
                 }
             }
+
             if (nextNode == null) {
                 // There's no unvisited neighboring Node, meaning that all neighboring Nodes have
-                // already been visited; we have reached a dead end.
+                // already been visited; we have reached a dead end; go back to the previous Node.
                 path.removeLast();
                 current = path.getLast();
             } else {
@@ -99,24 +102,19 @@ public class Path {
     public static Path findShortestPathByName(String fileName, String node1Name, String node2Name) {
         Path path1 = findPath(fileName, node1Name, node2Name);
         Path path2 = findPath(fileName, node2Name, node1Name);
-        double path1Distance = path1.findTotalDistance();
-        double path2Distance = path2.findTotalDistance();
+        double path1Distance = Node.roundToTwoDecimalPlaces(path1.findTotalDistance());
+        double path2Distance = Node.roundToTwoDecimalPlaces(path2.findTotalDistance());
         System.out.println(path1 + "; distance: " + path1Distance);
         System.out.println(path2 + "; distance: " + path2Distance);
-        Path shortest;
-        if (path2Distance < path1Distance)
-            shortest = path2.getReversedPath();
-        else
-            shortest = path1;
-        System.out.println("Shortest: " + shortest + "; distance: " + shortest.findTotalDistance() + "\n");
+        Path shortest = path2Distance < path1Distance ? path2.getReversedPath() : path1;
+        System.out.println("Shortest: " + shortest + "; distance: " + Node.roundToTwoDecimalPlaces(shortest.findTotalDistance()) + "\n");
         return shortest;
     }
 
     private Path getReversedPath() {
         LinkedList<Node> reversed = new LinkedList<>();
-        for (int i = path.size() - 1; i >= 0; i--) {
+        for (int i = path.size() - 1; i >= 0; i--)
             reversed.add(path.get(i));
-        }
         return new Path(reversed);
     }
 
@@ -134,19 +132,6 @@ public class Path {
             String node2 = info[1];
             findShortestPathByName(fileName, node1, node2);
         }
-
-        /*
-        0 1 2 3 4 5 6 7 8    <- x
-        1 A - B ----L
-        2 \     \       F
-        3  \      C --/
-        4   D --/   \
-        5   |\    G - E
-        6   | H -/     \I
-        7   J   \-- K
-        8     \-M -/
-
-        ^y
-        */
     }
 }
+
