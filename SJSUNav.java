c24c8060c4e2855849430224a44fe874ef9994a8
mdl00 <mdleeper@gmail.com>
2021-04-29 13:04:23 -0700

    Update SJSUNav.java

--- SJSUNav.java
+++ SJSUNav.java
@@ -12,7 +12,34 @@ import javax.imageio.ImageIO;
 
 class MapImage extends Canvas {
 	
-	static JButton[][] gridButtons = new JButton[128][128];
+	JButton[][] gridButtons = new JButton[128][128];
+	
+	int xStart = -1, yStart = -1;
+	int xEnd = -1, yEnd = -1;
+	
+	public JButton[][] getButtons() {
+		return gridButtons;
+	}
+	
+	public void setStart(int x, int y) {
+		xStart = x;
+		yStart = y;
+	}
+	
+	public void setEnd(int x, int y) {
+		xEnd = x;
+		yEnd = y;
+	}
+	
+	public boolean hasStart() {
+		if (xStart != -1) return true;
+		return false;
+	}
+	
+	public boolean hasEnd() {
+		if (xEnd != -1) return true;
+		return false;
+	}
 	
 	public void paint(Graphics g) {  
 		
@@ -54,10 +81,7 @@ class MapImage extends Canvas {
         	
 			for (int i = 0; i < 128; i++) {
 	        	for (int j = 0; j < 128; j++) {
-	        		JButton button = new JButton();
-        			button.setBounds(i*5, (128-j)*4, 5, 4);
-        			button.setVisible(false);
-	        		gridButtons[i][j] = button;
+	        		gridButtons[i][j] = null;
 	        	}
 			}
         	while (scan.hasNextLine()) {
@@ -66,16 +90,34 @@ class MapImage extends Canvas {
         		if (info[0].equals("New")) {
         			xCoord = Integer.parseInt(info[2]);
         			yCoord = Integer.parseInt(info[3]);
-        			g.setColor(Color.MAGENTA);
+        			if (xCoord == xStart && yCoord == yStart) {
+        				g.setColor(Color.GREEN);
+        			} else if (xCoord == xEnd && yCoord == yEnd) {
+        				g.setColor(Color.RED);
+        			} else {
+        				g.setColor(Color.MAGENTA);
+        			}
         			g.fillRect(xCoord*5 + 32, (128-yCoord)*4 + 174, 5, 4);
         			g.setColor(Color.BLACK);
         			g.drawRect(xCoord*5 + 32, (128-yCoord)*4 + 174, 5, 4);
-        			gridButtons[xCoord][yCoord].setIcon(ticon);
+        			JButton button = new JButton(ticon);
+        			button.setBounds(xCoord*5, (128-yCoord)*4, 5, 4);
+        			button.setVisible(false);
+	        		gridButtons[xCoord][yCoord] = button;
         		}
         	}
         	scan.close();
         } catch (Exception exception) {
+        	System.out.print("Stack exception: ");
             exception.printStackTrace();
+        }
+        
+        for (int i = 0; i < 128; i++) {
+        	for (int j = 0; j < 128; j++) {
+        		if (gridButtons[i][j] != null) {
+        			System.out.println(i + ", " + j);
+        		}
+        	}
         }
     }
 }
@@ -84,56 +126,24 @@ public class SJSUNav {
 	
 	public enum Direction{LEFT, RIGHT, UP, DOWN};
 	
-	static int startX = -1;
-	static int endX = -1;
-	static int startY = -1;
-	static int endY = -1;
-	
-	final static String GREEN_PATH = "/Users/matthew/Downloads/greenblock.png";
-	final static String TGREEN_PATH = "/Users/matthew/Downloads/transparentgreenblock.png";
-	final static String RED_PATH = "/Users/matthew/Downloads/redblock.png";
-	final static String TRED_PATH = "/Users/matthew/Downloads/transparentredblock.png";
-	final static String TBLUE_PATH = "/Users/matthew/Downloads/transparentblueblock.png";
-	final static String T_PATH = "/Users/matthew/Downloads/transparentblock.png";
+	int startX = -1;
+	int endX = -1;
+	int startY = -1;
+	int endY = -1;
 	
 	public static void main(String args[]) {
 		Direction d = Direction.LEFT;
-		
-		try {
-		BufferedImage bimg = ImageIO.read(new File(GREEN_PATH));
-		final ImageIcon greenicon = new ImageIcon(bimg);
-		bimg = ImageIO.read(new File(TGREEN_PATH));
-		final ImageIcon tgreenicon = new ImageIcon(bimg);
-		bimg = ImageIO.read(new File(RED_PATH));
-		final ImageIcon redicon = new ImageIcon(bimg);
-		bimg = ImageIO.read(new File(TRED_PATH));
-		final ImageIcon tredicon = new ImageIcon(bimg);
-		bimg = ImageIO.read(new File(T_PATH));
-		final ImageIcon ticon = new ImageIcon(bimg);
-		bimg = ImageIO.read(new File(TBLUE_PATH));
-		final ImageIcon tblueicon = new ImageIcon(bimg);
-		} catch (Exception exception) {
-            exception.printStackTrace();
-        }
 		
 		JFrame mainWindow = new JFrame();
 		
 		try {
 			MapImage mi = new MapImage();
 			
-			/*JButton[][] gridButtons = new JButton[128][128];
-			for (int i = 0; i < 128; i++) {
-	        	for (int j = 0; j < 128; j++) {
-	        		
-	        		gridButtons[i][j] = button;
-	        	}
-	        }
-			
 			for (int i = 0; i < 128; i++) {
 				for (int j = 0; j < 128; j++) {
-					mainWindow.add(gridButtons[i][j]);
+					if (mi.getButtons()[i][j] != null) mainWindow.add(mi.getButtons()[i][j]);
 				}
-			}*/
+			}
 						
 			mi.addMouseListener(new MouseAdapter() {
 				
@@ -149,7 +159,22 @@ public class SJSUNav {
 		        	x = (x-32)/5;
 		        	y = 183 - y/4;
 		        	System.out.println(x + ", " + y);
-					if (MapImage.gridButtons[x][y].getIcon() == ticon);
+		        	if (x < 128 && y < 128) {
+		        		if (mi.getButtons()[x][y] != null) {
+		        			if (mi.hasStart() && mi.hasEnd()) {
+		        				mi.setStart(x, y);
+		        				mi.setEnd(-1, -1);
+		        			} else if (!mi.hasStart()) {
+		        				mi.setStart(x, y);
+		        			} else if (!mi.hasEnd()) {
+		        				mi.setEnd(x, y);
+		        			}
+						} else {
+							mi.setStart(-1, -1);
+							mi.setEnd(-1, -1);
+						}
+		        		mi.repaint();
+		        	}
 	            }
 	        });
 		
